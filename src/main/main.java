84d10145ee149245d3a70eb3d1415f098028363b
douaa zeghmar <douaa.zeghmar@univ-alger.com>
2025-10-23 21:24:03 +0100

    Created project packages and Main.java base

---
+++ src/main/main.java
@@ -0,0 +1,13 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class main {
+    public static void main(String[] args) {
+        //
+        JFrame frame = new JFrame("ElectroMart - Electronics Store");
+        frame.setSize(800, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
